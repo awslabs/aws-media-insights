294aed86f2ca0a8c384b73f42433b56b6bf5a586
ianwow <ianwow@amazon.com>
2021-04-25 22:46:20 -0700

    fix logic to check if stack exists

--- test/app.js
+++ test/app.js
@@ -12,51 +12,55 @@ let temp_password = process.env.TEMP_PASSWORD;
     })
 
     const test_collection_screenshot = await ScreenshotTester(
-      0.1, // threshold
-      false, // anti-aliasing
-      false, // ignore colors
-      {
-          ignoreRectangles: [[36, 335, 169, 106], [649, 345, 372, 54], [568, 428, 170, 85] ],
-      }, // rectangles
-      {
-          transparency: 0.5
-      }
+        0.1, // threshold
+        false, // anti-aliasing
+        false, // ignore colors
+        {
+            ignoreRectangles: [[36, 335, 169, 106], [649, 345, 372, 54], [568, 428, 170, 85]],
+        }, // rectangles
+        {
+            transparency: 0.5
+        }
     )
 
     const test_screenshot_containing_video = await ScreenshotTester(
-      0.1, // threshold
-      false, // anti-aliasing
-      false, // ignore colors
-      {
-          ignoreRectangles: [[670, 95, 583, 331], [648, 657, 403, 27]],
-      }, // rectangles
-      {
-          transparency: 0.5
-      }
+        0.1, // threshold
+        false, // anti-aliasing
+        false, // ignore colors
+        {
+            ignoreRectangles: [[670, 95, 583, 331], [648, 657, 403, 27]],
+        }, // rectangles
+        {
+            transparency: 0.5
+        }
     )
 
     const test_words_screenshot = await ScreenshotTester(
-      0.1, // threshold
-      false, // anti-aliasing
-      false, // ignore colors
-      {
-          ignoreRectangles: [[670, 95, 583, 331], [648, 657, 403, 27], [15, 284, 610, 70]],
-      }, // rectangles
-      {
-          transparency: 0.5
-      }
+        0.1, // threshold
+        false, // anti-aliasing
+        false, // ignore colors
+        {
+            ignoreRectangles: [[670, 95, 583, 331], [648, 657, 403, 27], [15, 284, 610, 70]],
+        }, // rectangles
+        {
+            transparency: 0.5
+        }
     )
 
     console.log("Loading " + url)
     // Chromium doesn't support video playback, so use Chrome instead
     // uncomment this when testing on a laptop:
     // const browser = await  puppeteer.launch({executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', headless: true})
-    const browser = await  puppeteer.launch({executablePath: '/usr/bin/google-chrome', headless: true, args: ['--no-sandbox']})
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/google-chrome',
+        headless: true,
+        args: ['--no-sandbox']
+    })
     const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 926 });
-    await page.goto(url,  {waitUntil: 'networkidle0'});
+    await page.setViewport({width: 1280, height: 926});
+    await page.goto(url, {waitUntil: 'networkidle0'});
     // Type in the username
-    await page.type('input','s3sink@bigendiandata.com')
+    await page.type('input', 's3sink@bigendiandata.com')
     // Type in the password
     await page.type('#app > div > div > div > div.Section__sectionBody___3DCrX > div:nth-child(2) > input', temp_password)
     // console.log("Validating auth form: "+await test_screenshot(page, 'screenshot00_authentication', {
@@ -67,47 +71,45 @@ let temp_password = process.env.TEMP_PASSWORD;
     console.log("Authenticating")
     // click Login button
     await Promise.all([
-      page.click("button"),
-      page.waitForTimeout(2000)
-      // page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        page.click("button"),
+        page.waitForTimeout(2000)
+        // page.waitForNavigation({ waitUntil: 'networkidle0' }),
     ]);
 
-      // Are we on the password reset page?
-      passwordHeaderSelector = "#app > div > div > div > div.Section__sectionHeader___13iO4"
-      try {
+    // Are we on the password reset page?
+    passwordHeaderSelector = "#app > div > div > div > div.Section__sectionHeader___13iO4"
+    try {
         if (await page.$(passwordHeaderSelector) !== null) {
-          console.log("found password reset form")
-          // enter new password
-          await page.type('#app > div > div > div > div.Section__sectionBody___3DCrX > div > input', temp_password)
-          console.log("submitting new password")
-          // click Submit button
-          await Promise.all([
-            page.click("button"),
-            page.waitForTimeout(2000)
-          ]);
+            console.log("found password reset form")
+            // enter new password
+            await page.type('#app > div > div > div > div.Section__sectionBody___3DCrX > div > input', temp_password)
+            console.log("submitting new password")
+            // click Submit button
+            await Promise.all([
+                page.click("button"),
+                page.waitForTimeout(2000)
+            ]);
         } else {
-          console.log('Temporary password has already been reset.')
+            console.log('Temporary password has already been reset.')
         }
-      } catch(e) {
+    } catch (e) {
         console.log('Temporary password has already been reset.')
-      }
+    }
 
-    // Wait for catalog view to load
-    console.log("Waiting 5s for catalog view to load")
     await page.waitForTimeout(5000)
 
     // Validate that the catalog table is not empty
     tableEmptyMessageSelector = "tbody > tr > td > div > div"
     try {
-      if (await page.$(tableEmptyMessageSelector) !== null) {
-        const text = await page.$eval(tableEmptyMessageSelector, el => el.textContent);
-        if (text === 'There are no records to show') {
-          throw new Error("There are no records to show.")
+        if (await page.$(tableEmptyMessageSelector) !== null) {
+            const text = await page.$eval(tableEmptyMessageSelector, el => el.textContent);
+            if (text === 'There are no records to show') {
+                throw new Error("There are no records to show.")
+            }
         }
-      }
     } catch (e) {
-      console.error(e)
-      process.exit(-1)
+        console.error(e)
+        process.exit(-1)
     }
 
     // Validate that the workflow status for the first asset in the
@@ -115,38 +117,44 @@ let temp_password = process.env.TEMP_PASSWORD;
     console.log("Validating that the first asset's workflow is Complete")
     const assetWorkflowStatusSelector = "td.tableWordWrap:nth-child(3) > a:nth-child(1)"
     try {
-      await page.waitForSelector(assetWorkflowStatusSelector, {polling:1000, timeout: 3000})
+        await page.waitForSelector(assetWorkflowStatusSelector, {polling: 1000, timeout: 3000})
     } catch (e) {
-      console.log('Missing expected asset workflow status.\n' + e)
+        console.log('Missing expected asset workflow status.\n' + e)
     }
     try {
-      const text = await page.$eval(assetWorkflowStatusSelector, el => el.textContent);
-      if (text !== 'Complete') {
-        console.log("Asset workflow status: " + text)
-        throw new Error("Asset workflow status is not Complete")
-      }
+        const text = await page.$eval(assetWorkflowStatusSelector, el => el.textContent);
+        if (text !== 'Complete') {
+            console.log("Asset workflow status: " + text)
+            throw new Error("Asset workflow status is not Complete")
+        }
     } catch (e) {
-      console.error(e)
-      process.exit(-1)
+        console.error(e)
+        process.exit(-1)
     }
 
     // VALIDATE OBJECTS TAB
     console.log("Loading objects tab.")
-    await page.click('tbody > tr:nth-child(1) > td:nth-child(6) > a');
-    // wait until video loads
-    const video_selector='#videoPlayer > div.vjs-control-bar > div.vjs-remaining-time.vjs-time-control.vjs-control > span.vjs-remaining-time-display'
-    await page.waitForSelector(video_selector,  {polling:1000, timeout: 5000})
-    // validate that object detection results are present
-    console.log("Validating that object detection results are present.")
-    const rounded_button_selector = '#app > div > div.container-fluid > div > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(2) > div > button:nth-child(2)'
     try {
-        if (await page.waitForSelector(rounded_button_selector) === null) {
-          throw new Error("Missing object detection results")
+        await page.click('tbody > tr:nth-child(1) > td:nth-child(6) > a');
+        // validate that the video loads
+        let video_selector = '#videoPlayer > div.vjs-control-bar > div.vjs-remaining-time.vjs-time-control.vjs-control > span.vjs-remaining-time-display'
+        await page.waitForSelector(video_selector, {polling: 1000, timeout: 5000})
+        // Validate that rounded buttons are present
+        let rounded_buttons_xpath = '//div/div[2]/div/div[1]/div[2]/div/div/div[2]/div/button'
+        if (await page.waitForXPath(rounded_buttons_xpath) === null) {
+            throw new Error("Missing results")
         }
-    } catch(e) {
-        console.log(e)
+        // Validate the number of rounded buttons matches the results summary statement
+        let rounded_button_count = await page.$x(rounded_buttons_xpath);
+        results_summary_selector = '#app > div > div.container-fluid > div > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(3) > div > p:nth-child(1)'
+        let results_summary = await page.$eval(results_summary_selector, el => el.innerText);
+        let validated = (results_summary.match(/\d+/g)[1] == rounded_button_count.length)
+        console.log("Results summary: " + results_summary)
+        if (!validated) throw new Error("Missing results")
+    } catch (e) {
+        console.error(e)
+        process.exit(-1)
     }
-
     // console.log("validating objects: "+await test_screenshot_containing_video(page, 'screenshot02_tab_objects',   {
     // fullPage: true,
     // }))
@@ -154,35 +162,50 @@ let temp_password = process.env.TEMP_PASSWORD;
 
     // VALIDATE CELEBRITY TAB
     let tab_selector='#__BVID__28___BV_tab_button__'
-    await page.click(tab_selector)
+    console.log("Loading celebrity tab.")
     try {
-        await page.waitForSelector(rounded_button_selector, {timeout: 5000})
-    } catch(e) {
-        // no data in this tab
+        await page.click(tab_selector);
+        // Validate that rounded buttons are present
+        rounded_buttons_xpath = '//div/div[2]/div/div[1]/div[2]/div/div/div[2]/div/button'
+        if (await page.waitForXPath(rounded_buttons_xpath) === null) {
+            throw new Error("Missing results")
+        }
+        // Validate the number of rounded buttons matches the results summary statement
+        rounded_button_count = await page.$x(rounded_buttons_xpath);
+        results_summary_selector = '#app > div > div.container-fluid > div > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(3) > div > p:nth-child(2)'
+        results_summary = await page.$eval(results_summary_selector, el => el.innerText);
+        let validated = (results_summary.match(/\d+/g)[1] == rounded_button_count.length)
+        console.log("Results summary: " + results_summary)
+        if (!validated) throw new Error("Missing results")
+    } catch (e) {
+        console.error(e)
+        process.exit(-1)
     }
     // console.log("validating celebrities: "+await test_screenshot_containing_video(page, 'screenshot03_tab_celebrities', {
     //     fullPage: true,
     // }))
 
-    // VALIDATE MODERATION TAB
-    tab_selector='#__BVID__30___BV_tab_button__'
-    await page.click(tab_selector)
-    try {
-        await page.waitForSelector(rounded_button_selector, {timeout: 5000})
-    } catch(e) {
-        // no data in this tab
-    }
-    // console.log("validating moderation: "+await test_screenshot_containing_video(page, 'screenshot04_tab_moderation', {
-    //     fullPage: true,
-    // }))
 
     // VALIDATE FACES TAB
     tab_selector='#__BVID__32___BV_tab_button__'
-    await page.click(tab_selector)
+    console.log("Loading faces tab.")
     try {
-        await page.waitForSelector(rounded_button_selector, {timeout: 5000})
-    } catch(e) {
-        // no data in this tab
+        await page.click(tab_selector);
+        // Validate that rounded buttons are present
+        rounded_buttons_xpath = '//div/div[2]/div/div[1]/div[2]/div/div/div[2]/div/button'
+        if (await page.waitForXPath(rounded_buttons_xpath) === null) {
+            throw new Error("Missing results")
+        }
+        // Validate the number of rounded buttons matches the results summary statement
+        rounded_button_count = await page.$x(rounded_buttons_xpath);
+        results_summary_selector = '#app > div > div.container-fluid > div > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(3) > div > p:nth-child(2)'
+        results_summary = await page.$eval(results_summary_selector, el => el.innerText);
+        let validated = (results_summary.match(/\d+/g)[1] == rounded_button_count.length)
+        console.log("Results summary: " + results_summary)
+        if (!validated) throw new Error("Missing results")
+    } catch (e) {
+        console.error(e)
+        process.exit(-1)
     }
     // console.log("validating faces: "+await test_screenshot_containing_video(page, 'screenshot05_tab_faces', {
     //     fullPage: true,
@@ -190,11 +213,24 @@ let temp_password = process.env.TEMP_PASSWORD;
 
     // VALIDATE WORDS TAB
     tab_selector='#__BVID__34___BV_tab_button__'
-    await page.click(tab_selector)
+    console.log("Loading words tab.")
     try {
-        await page.waitForSelector(rounded_button_selector, {timeout: 3000})
-    } catch(e) {
-        // no data in this tab
+        await page.click(tab_selector);
+        // Validate that rounded buttons are present
+        rounded_buttons_xpath = '//div/div[2]/div/div[1]/div[2]/div/div/div[2]/div/button'
+        if (await page.waitForXPath(rounded_buttons_xpath) === null) {
+            throw new Error("Missing results")
+        }
+        // Validate the number of rounded buttons matches the results summary statement
+        rounded_button_count = await page.$x(rounded_buttons_xpath);
+        results_summary_selector = '#app > div > div.container-fluid > div > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(3) > div > p:nth-child(1)'
+        results_summary = await page.$eval(results_summary_selector, el => el.innerText);
+        let validated = (results_summary.match(/\d+/g)[1] == rounded_button_count.length)
+        console.log("Results summary: " + results_summary)
+        if (!validated) throw new Error("Missing results")
+    } catch (e) {
+        console.error(e)
+        process.exit(-1)
     }
     // console.log("validating words: "+await test_words_screenshot(page, 'screenshot06_tab_words', {
     //     fullPage: true,
